60342020ffdc2582e861dd3e886c329df3557ed0
alumno <alumno@vm1>
2022-01-14 08:27:43 +0100

    Controlador de recursos del modelo Centro

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,31 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
+use App\Http\Controllers\API\CentroController;
+
+Route::apiResource('centros', CentroController::class);
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+        'address' => env('DB_HOST', '127.0.0.1'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})->where('any', '.*');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+
 });
+
+
+
