9bd9672d3d20674da3ee52aa7ed6f733ece030e3
alumno <alumno@vm1>
2022-01-17 08:18:52 +0100

    Añadiendo el controlador de recursos a la tabla niveles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\NivelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::apiResource('Nivel', NivelController::class);
 
 Route::get('/', function () {
     return view('welcome');
